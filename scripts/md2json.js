169d3a9407df35dea32f5a0cee6a5097fbbceaab
Alex <alexnezhynsky@gmail.com>
2018-12-01 17:17:32 -0500

    Enable recursive lookup for .md

--- scripts/md2json.js
+++ scripts/md2json.js
@@ -1,7 +1,7 @@
 // https://unified.js.org/using-unified.html
 // https://www.npmjs.com/package/unified (used by react-markdown)
 
-const fs = require('fs')
+const fs = require('fs-extra')
 const dir = require('node-dir')
 const unified = require('unified')
 const markdown = require('remark-parse')
@@ -11,42 +11,33 @@ const html = require('rehype-stringify')
 const input = 'content'
 const output = `build/json`
 
-if (!fs.existsSync(output)) {
-  console.log(`📁  Make /${output}\n`)
-  fs.mkdirSync(output, { recursive: true })
-}
+fs.ensureDir(output)
+  .then(() => {
+    process.chdir(input)
 
-console.log(`📂  Change to /${input}`)
-process.chdir(input)
+    const processor = unified()
+      .use(markdown)
+      .use(remark2rehype)
+      .use(html)
 
-const processor = unified()
-  .use(markdown)
-  .use(remark2rehype)
-  .use(html)
-
-console.log(`\n⌚  Loop through files`)
-dir.readFiles('.',
-  { match: /.md$/ },
-  (err, content, filename, next) => {
-    if (err) throw err
-
-    console.log(`\n📝  Convert ${filename}`)
-    processor.process(content, (err, { contents }) => {
-      if (err) throw err
-
-      const [ name ] = filename.split('.md')
-
-      const json = JSON.stringify({ contents })
-      const file = `${name}.json`
-
-      console.log(`\n💽  Save ${file}`)
-      fs.writeFile(`../${output}/${file}`, json, 'utf8', err => {
+    dir.readFiles('.',
+      { match: /.md$/ },
+      (err, content, filename, next) => {
         if (err) throw err
-      })
-    })
 
-    next()
-  }, (err, files) => {
-    if (err) throw err
-    console.log('\n 🎉 Done!')
+        processor.process(content, (err, { contents }) => {
+          if (err) throw err
+
+          const [ fileNoExt ] = filename.split('.md')
+          const path = `../${output}/${fileNoExt}.json`
+
+          fs.outputJson(path, { contents })
+        })
+
+        next()
+      }, (err, files) => {
+        if (err) throw err
+        console.log('Converted MD to JSON')
+      })
   })
+  .catch(console.error)
